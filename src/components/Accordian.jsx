293d4a35b2bf7bb1d881ff773e1b2938ae10a32e
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-02-03 14:51:51 +0530

    :lipstick: Accordian using css

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,13 +1,32 @@
 /* eslint-disable react/prop-types */
 
-const Accordian = ({ id, title, description, handleToggle, isOpen }) => {
+const Accordian = ({ id, title, description }) => {
   return (
-    <div className="accordian__wrapper">
-      <div className="accordian__header" onClick={() => handleToggle(id)}>
-        <h3 className="accordian__title">{title}</h3>
-        <i className="accordian__icon">{isOpen ? "-" : "+"}</i>
+    // <div className="accordian__wrapper">
+    //   <div className="accordian__header" onClick={() => handleToggle(id)}>
+    //     <h3 className="accordian__title">{title}</h3>
+    //     <i className="accordian__icon">{isOpen ? "-" : "+"}</i>
+    //   </div>
+    //   {isOpen && <div className="accordian__body">{description}</div>}
+    // </div>
+
+    <div className="accordian">
+      <input
+        type="checkbox"
+        name="accordian-title"
+        className="accordian-checkbox"
+        id={`accordian-title-${id}`}
+      />
+      <label
+        htmlFor={`accordian-title-${id}`}
+        className="accordian-title-label"
+      >
+        {title}
+        <i className="icon"></i>
+      </label>
+      <div className="accordian-content">
+        <p>{description}</p>
       </div>
-      {isOpen && <div className="accordian__body">{description}</div>}
     </div>
   );
 };
---
+++ src/constants/Accoridans.jsx
@@ -0,0 +1,30 @@
+export const ACCORDIAN_DATA = [
+  {
+    id: 1,
+    title: "Accordian 1",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi corporis quidem illum sed, molestiae nulla eaque harum vitae? Cupiditate, animi?",
+    isOpen: false,
+  },
+  {
+    id: 2,
+    title: "Accordian 2",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi corporis quidem illum sed, molestiae nulla eaque harum vitae? Cupiditate, animi?",
+    isOpen: false,
+  },
+  {
+    id: 3,
+    title: "Accordian 3",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi corporis quidem illum sed, molestiae nulla eaque harum vitae? Cupiditate, animi?",
+    isOpen: false,
+  },
+  {
+    id: 4,
+    title: "Accordian 4",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi corporis quidem illum sed, molestiae nulla eaque harum vitae? Cupiditate, animi?",
+    isOpen: false,
+  },
+];
